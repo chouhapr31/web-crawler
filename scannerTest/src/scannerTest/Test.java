e0e6cfe28328a858ccc3e5718eacae7db5ae1641
chouhapr <prashantsingh3153@gmail.com>
2018-09-27 12:48:43 +0530

    afds

--- scannerTest/src/scannerTest/Test.java
+++ scannerTest/src/scannerTest/Test.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Test {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println(sc);
+		System.out.println("Give URL");
+		System.out.println(sc.next());
 	}
 }
